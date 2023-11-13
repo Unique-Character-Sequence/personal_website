b9e740bd3e64700f29b0e9e0130a1c4daaf4e8f5
Pilici Rustam <paroli7328129ad@gmail.com>
2023-11-13 15:48:52 +0200

    [+] typing animation [+] hover animations and buttons

--- src/app/HeroSection/HeroSection.tsx
+++ src/app/HeroSection/HeroSection.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image"
+import "./animate_gradient.scss"
+import { TypeAnimation } from "react-type-animation"
 
 const HeroSection = () => {
     return (
@@ -6,16 +10,35 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        Hello, I&apos;m Pilici Rustam
+                        <div className="mb-2 animate-gradient text-transparent bg-clip-text bg-gradient-to-br from-teal-200 via-indigo-400 via-40% to-teal-200">
+                            Hello, I&apos;m{" "}
+                        </div>
+                        <TypeAnimation
+                            className="whitespace-nowrap relative sm:absolute z-10"
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Pilici Rustam',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Frontend Developer',
+                                1000,
+                                'Web Developer',
+                                1000,
+                                'UI/UX Developer',
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
                     </h1>
-                    <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+                    <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6 mt-14 lg:mt-20">
                         And I am your perfect match, consectetur adipisicing elit.
-                        Possimus similique asperiores, molestias temporibus, nulla vel,
+                        Possimus similique asperiores, molestias temporibus, nullvel,
                         saepe sint assumenda nostrum iste eveniet porro modi voluptatibus
                         velit a veniam? Ut, repudiandae voluptas.
                     </p>
                     <div>
-                        <button className="sm:w-fit w-full rounded-full px-6 py-3 mr-4 transition-all duration-200 bg-center bg-[length:300%_100%] hover:bg-right bg-gradient-to-br from-blue-500 via-purple-500 to-lime-500 text-white">Hire Me</button>
+                        <button className="sm:w-fit w-full rounded-full px-6 py-3 mr-4 transition-all duration-200 bg-center bg-[length:300%_100%] hover:bg-right bg-gradient-to-br from-indigo-500 via-red-600 via-75% hover:via-45% to-yellow-400 text-white">Hire Me</button>
                         <button className="sm:w-fit w-full px-[22px] py-2.5 mt-3 border-2 bg-transparent hover:bg-sky-200 hover:text-black transition duration-200 text-white rounded-full">Download CV</button>
                     </div>
                 </div>
