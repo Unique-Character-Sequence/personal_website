15c96c7ab169c941f83f7a625d74010cfe3e36da
Pilici Rustam <paroli7328129ad@gmail.com>
2023-11-15 15:27:50 +0200

    styling fix

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 
-const hoverMenuItem = "hover:text-white transition duration-150"
+const hoverMenuItem = "hover:text-white hover:text-underline transition duration-150"
 
 const Navbar = () => {
     return (
-        <nav className="fixed w-full bg-[#121212] z-20 sm:text-xl lg:text-2xl">
-            <div className="flex flex-wrap items-center mx-auto px-14 py-2 justify-between">
+        <nav className="fixed w-full bg-[#121212] bg-opacity-95 z-20 sm:text-xl lg:text-2xl">
+            <div className="flex flex-wrap items-center mx-auto px-[8vw] py-3.5 justify-between">
                 <Link href={"/"} className="text-white">Logo</Link>
-                <div className="hidden sm:flex justify-between text-neutral-300 w-[220px] sm:w-[260px] md:w-[340px] lg:-[300px]">
+                <div className="hidden sm:flex justify-between text-neutral-300 w-[220px] sm:w-[260px] md:w-[300px] lg:w-[380px]">
                     <Link className={hoverMenuItem} href={"#about"}>About</Link>
                     <Link className={hoverMenuItem} href={"#projects"}>Projects</Link>
                     <Link className={hoverMenuItem} href={"#contacts"}>Contacts</Link>
