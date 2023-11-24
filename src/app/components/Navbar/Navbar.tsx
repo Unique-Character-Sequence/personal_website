77f2c0bff333924ac7d3e122922911b63f74b0c2
Pilici Rustam <paroli7328129ad@gmail.com>
2023-11-24 12:54:17 +0200

    mobile hamburger navigation

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import "./animate_appearing.scss"
 import Link from "next/link"
 import { useCallback, useEffect, useState } from "react";
 import Image from "next/image"
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { throttle } from "@/app/utils/utils";
 
 const hoverMenuItem = "hover:text-white transition duration-150"
@@ -15,19 +15,17 @@ const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     const handleScrollNoThrottle = () => {
-        console.log("qweqwe");
-
         setPrevScrollPos((prevScrollPos) => {
             const currentScrollPos = window.scrollY;
             if (currentScrollPos > prevScrollPos) {
                 setVisible(false);
+                setIsMenuOpen(false);
             } else {
                 setVisible(true);
             }
             return currentScrollPos; // Update the state with the current value
         });
     };
-
 
     const handleScroll = useCallback(throttle(handleScrollNoThrottle, 200), []);
 
@@ -36,15 +34,15 @@ const Navbar = () => {
         return () => window.removeEventListener('scroll', handleScroll)
     }, [handleScroll])
 
-    const handleHamClick = (): void => {
-        setIsMenuOpen(true);
+    const handleHamClick = (isOpen: boolean): void => {
+        setIsMenuOpen(isOpen);
     };
 
     return (
         <nav id="navbar" className={`appearing_transition ${visible ? 'top-0' : 'top-[-100px]'} sticky w-full bg-[#121212] bg-opacity-95 z-20`}>
             <div className="min-h-[60px] relative transition duration-300 flex flex-wrap items-center mx-auto px-[8vw] py-3.5 justify-between  sm:text-xl lg:text-2xl">
                 <Link href={"/"} className="text-white">
-                    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 bg-[#252525] w-[50px] h-[50px] flex justify-center rounded-full transition duration-200 hover:scale-105">
+                    <div className="relative bg-[#252525] w-[50px] h-[50px] flex justify-center rounded-full transition duration-200 hover:scale-105">
                         <Image
                             src="/images/Memoji_Boy.png"
                             alt="my memoji"
@@ -57,8 +55,17 @@ const Navbar = () => {
                     <Link className={hoverMenuItem} href={"#projects"}>Projects</Link>
                     <Link className={hoverMenuItem} href={"#contacts"}>Contacts</Link>
                 </div>
-                <Bars3Icon className="w-10 h-10 sm:hidden text-neutral-300 cursor-pointer" onClick={handleHamClick} />
+                {isMenuOpen
+                    ? <XMarkIcon className="w-10 h-10 sm:hidden text-neutral-300 cursor-pointer" onClick={() => handleHamClick(false)} />
+                    : <Bars3Icon className="w-10 h-10 sm:hidden text-neutral-300 cursor-pointer" onClick={() => handleHamClick(true)} />
+                }
+            </div>
 
+
+            <div className={`${isMenuOpen === false && "hidden"} transition-all duration-400 text-neutral-300 flex flex-col items-center`}>
+                <Link className={hoverMenuItem + " text-2xl"} href={"#about"}>About</Link>
+                <Link className={hoverMenuItem + " " + "py-8 text-2xl"} href={"#projects"}>Projects</Link>
+                <Link className={hoverMenuItem + " " + "pb-2 text-2xl"} href={"#contacts"}>Contacts</Link>
             </div>
         </nav>
     )
