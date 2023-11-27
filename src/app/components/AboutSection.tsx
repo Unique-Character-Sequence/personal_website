f906d5c1d4d5e27da2aeff993c554add9db8773e
Pilici Rustam <paroli7328129ad@gmail.com>
2023-11-27 19:08:48 +0200

    developing "about me" part

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,36 +1,63 @@
-import React from 'react'
 import Image from 'next/image'
 
 const AboutSection = () => {
     return (
-        <section className='text-white'>
-            <div className="grid grid-cols-12 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <div className="flex col-span-4 justify-center md:items-center items-start">
-                    <Image
-                        src="/images/me.png"
-                        alt="my memoji"
-                        height={280}
-                        width={280} />
+        <section className='text-white mt-6'>
+            <div className="px-4 lg:px-16 xl:px-[8vw] text-justify text-md md:text-lg">
+                <div className='flex justify-center items-center'>
+                    <div className='w-11/12 bg-neutral-400 h-[2px]' />
+                    <h1 className='w-full mx-8 text-center py-10 font-bold text-2xl'>
+                        About&nbsp;Me
+                    </h1>
+                    <div className='w-11/12 bg-neutral-400 h-[2px]' />
                 </div>
-                <div className="col-span-8 text-md md:text-lg">
-                    <p>
-                        <s>Experienced React Developer specializing in...</s>
-                    </p>
+                <Image
+                    className="rounded-xl mr-4 w-44 md:w-60 float-left"
+                    src="/images/me.png"
+                    alt="my memoji"
+                    height={280}
+                    width={280} />
+                <p>
+                    <s>Hello, I&apos;m a React Developer specializing in...</s>
+                </p>
+                <br />
+                <p>
+                    Tired of the same old intros? Well, here&apos;s something different – I&apos;m not your typical <code>React Developer</code>. Yes, I bring hands-on experience with a diverse toolkit, including React, Redux, TypeScript, TailwindCSS, and more.
+                </p>
+                <br />
+                <p>
+                    <b>What truly sets me apart?</b>
                     <br />
-                    <p>
-                        Tired of the same old intros? Well, here&apos;s something different – I&apos;m not your typical <code>React Developer</code>. Yes, I bring hands-on experience with a diverse toolkit, including React, Redux, TypeScript, TailwindCSS and more.
-                    </p>
+                    Rapid growth.
+                    <br /><br />
+                    As you read this, I&apos;m actively enhancing my skills and expanding my toolkit to meet market demands. As a <strong>strong junior</strong>, my skills grow daily, ensuring adaptability.
+                </p>
+                <br />
+                <div className="text-left">
+                    {/* TODO: Cюда мы и засовываем доказательство, подкрепляя наши слова */}
+                    <h1 className="text-xl font-bold">Recent Achievements</h1>
+                    <b>Productivity</b>
+                    <ul>
+                        <li>- Achieved an impressive <strong>~50%</strong> boost in average productivity, utilizing advanced time management techniques.</li>
+                        <li>- Realized a significant <strong>~20%</strong> acceleration in Redux development speed through a seamless transition to Redux Toolkit.</li>
+                        <li>- Successfully achieved at least a twofold improvement in reducing debugging time by leveraging ChatGPT for thorough code reviews.</li>
+                        <li>- Optimized the construction of website layouts with or without pre-made designs by mastering Firefox Developer Edition tools.</li>
+                    </ul>
                     <br />
-                    <p>
-                        <b>What truly sets me apart?</b>
-                        <br />
-                        Rapid growth. As you read this, I&apos;m actively enhancing my skills and expanding my toolkit to meet evolving web development demands. I&apos;m not just an asset; I am <b>a strategic investment for your team</b>. My skills evolve daily, ensuring seamless alignment with the dynamic landscape. Invest in me, and position your team strategically for success in today&apos;s competitive market.
-                    </p>
-                    <br />
-                    <p>
-                        And hey, about salary expectations? Well, let&apos;s just say, my skills aren&apos;t the only thing growing every day!
-                    </p>
+                    <b>Tool Proficiency</b>
+                    <ul>
+                        <li>- Mastered <strong>SASS</strong> for creative styling, successfully applied in a project.</li>
+                        <li>- Seamlessly integrated <strong>TailwindCSS</strong> for streamlined and responsive designs.</li>
+                        <li>- Initiated <strong>Next.JS</strong> development for this website, embracing advanced technology.</li>
+                        <li>- Methodically documented bugs, implemented inventive solutions, and provided valuable feature insights.</li>
+                    </ul>
                 </div>
+                <br /><br />
+                Hiring me means not just immediate value but also the promise of a <strong>loyal middle developer</strong> and potential <strong>senior leader</strong> down the line. Invest in me today and get a strong asset for lasting success in a competitive market.
+                <br />
+                <p>
+                    And hey, about salary expectations? Well, let&apos;s just say, my skills aren&apos;t the only thing growing every day!
+                </p>
             </div>
         </section>
     )
--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ import { TypeAnimation } from "react-type-animation"
 const HeroSection = () => {
     return (
         <section>
-            <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="grid grid-cols-12 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-left">
                     <div>
                         <h1 className="text-white mb-4 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold">
@@ -36,23 +36,29 @@ const HeroSection = () => {
                     </div>
                     <p className="text-[#ADB7BE] text-sm sm:text-lg lg:text-xl mb-6">
                         Still looking for a skilled React developer?<br />Your search ends here.
-                        {/* TODO: Кнопки снизу тут рановато так-то стоят. Сначала флексишь, потом просишь нанять  */}
                     </p>
-                    <div>
+
+                    <div className="col-span-12 hidden sm:block">
+                        {/* TODO: Кнопки снизу тут рановато так-то стоят. Сначала флексишь, потом просишь нанять  */}
                         <button className="sm:w-fit w-full rounded-full px-6 py-3 mr-4 transition-all duration-200 bg-center bg-[length:300%_100%] hover:bg-right bg-gradient-to-br from-indigo-500 via-red-600 via-75% hover:via-45% to-yellow-400 text-white">Hire Me</button>
                         <button className="sm:w-fit w-full px-[22px] py-2.5 mt-3 border-2 bg-transparent hover:bg-sky-200 hover:text-black transition duration-200 text-white rounded-full">Download CV</button>
                     </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] sm:ml-12 w-[250px] lg:w-[312px] h-[250px] lg:h-[312px] relative">
+                    <div className="rounded-full bg-[#181818] sm:ml-12 w-[120px] h-[120px] sm:w-[250px] sm:h-[250px] lg:w-[312px] lg:h-[312px] relative">
                         <Image
-                            className="absolute w-[200px] lg:w-[250px] hover:scale-105 hover:rotate-6 transition duration-200 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            className="absolute w-[95px] sm:w-[200px] lg:w-[250px] hover:scale-105 hover:rotate-6 transition duration-200 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             src="/images/Memoji_Boy.png"
                             alt="my memoji"
                             height={250}
                             width={250}
                         />
                     </div>
+                </div>
+                <div className="col-span-12 flex sm:hidden">
+                    {/* TODO: Кнопки снизу тут рановато так-то стоят. Сначала флексишь, потом просишь нанять  */}
+                    <button className="sm:w-fit w-1/2 rounded-full px-6 py-3 mt-3 mr-4 transition-all duration-200 bg-center bg-[length:300%_100%] hover:bg-right bg-gradient-to-br from-indigo-500 via-red-600 via-75% hover:via-45% to-yellow-400 text-white">Hire Me</button>
+                    <button className="sm:w-fit w-1/2 px-[22px] py-2.5 mt-3 border-2 bg-transparent hover:bg-sky-200 hover:text-black transition duration-200 text-white rounded-full">Download CV</button>
                 </div>
             </div>
         </section>
